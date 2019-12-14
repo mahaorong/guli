a16c80397bdbde422eb38989250eac2e089700fa
15179631711 <xiao15179631711>
2019-12-14 13:18:58 +0800

    12.14 搜索项目第一次提交 xty

--- gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
+++ gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
@@ -1,4 +1,16 @@
 package com.atguigu.gmall.search.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SearchController {
+    @RequestMapping("/search")
+    @ResponseBody
+   public String search(){
+        System.out.println("修改");
+       return"搜索成功";
+   }
 }
