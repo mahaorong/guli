15b098375bd72e6df9c997d455dc8f1d86babc8d
15179631711 <xiao15179631711>
2019-12-14 13:30:37 +0800

    Merge branch 'master' of https://github.com/mahaorong/guli
    # Conflicts:
    #	gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java

--- gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
+++ gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
@@ -1,12 +1,6 @@
 package com.atguigu.gmall.search.controller;
 
 
-<<<<<<<<< Temporary merge branch 1
-public class SearchController {
-    public static void main(String[] args) {
-        System.out.println("搜索成功");
-    }
-=========
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,10 +9,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SearchController {
     @RequestMapping("/search")
     @ResponseBody
-   public String search(){
+    public String search() {
         System.out.println("修改");
         System.out.println("1.0");
         System.out.println("2.0");
-       return"搜索成功";
-   }
+        return "搜索成功";
+    }
 }
+
