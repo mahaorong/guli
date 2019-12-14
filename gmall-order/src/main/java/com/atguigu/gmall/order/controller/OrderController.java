944e4fb0b4259d13a5db353a865e7761525c919f
SA <1030853117@qq.com>
2019-12-14 13:18:22 +0800

    order  peng  2019.12.14  13:18

--- gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
+++ gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
@@ -12,4 +12,9 @@ public class OrderController {
     public String index() {
         return "index";
     }
+
+
+    public static void main(String[] args) {
+        System.out.println("我是小彭，我负责订单模块");
+    }
 }
