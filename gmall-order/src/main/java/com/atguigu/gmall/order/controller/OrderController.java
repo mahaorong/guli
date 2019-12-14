9a71bee8734f915b22b2cc1927cfc3eedcce10dc
SA <1030853117@qq.com>
2019-12-14 13:29:54 +0800

    order  peng  2019.12.14  13:18  我是小彭

--- gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
+++ gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
@@ -17,5 +17,7 @@ public class OrderController {
     public static void main(String[] args) {
         System.out.println("我是小彭，我负责订单模块");
         System.out.println(123456789);
+
+        System.out.println("来打我啊 撕逼");
     }
 }
