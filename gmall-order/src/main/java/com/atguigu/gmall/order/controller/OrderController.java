c2d7ec6f0ca8832a908b68182ff040fc7ec40410
mahaorong <931380187@qq.com>
2019-12-14 10:48:48 +0800

    12.14 项目启动了！！ mhr

--- gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
+++ gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
@@ -1,7 +1,15 @@
 package com.atguigu.gmall.order.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OrderController {
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
 }
