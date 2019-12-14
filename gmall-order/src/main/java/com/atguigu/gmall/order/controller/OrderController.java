85522e81c44b737d38c3223b0fa77d36f35f6911
SA <1030853117@qq.com>
2019-12-14 17:07:40 +0800

    性感小彭回来了

--- gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
+++ gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
@@ -10,16 +10,13 @@ public class OrderController {
     @RequestMapping("index")
     @ResponseBody
     public String index() {
-        System.out.println("小彭是猪");
+
         return "index";
     }
 
 
     public static void main(String[] args) {
-        System.out.println("小彭是猪");
-        System.out.println("我是小彭，我负责订单模块");
-        System.out.println(123456789);
 
-        System.out.println("来打我啊 撕逼");
+        System.out.println("李福鑫  撕逼");
     }
 }
